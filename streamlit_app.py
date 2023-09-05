5cd893a4656dbe3d305bce3de545b6c1044a2512
wasim-codeit <134153550+wasim-codeit@users.noreply.github.com>
2023-09-05 18:03:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My hobby is to travel the world')
 
-streamlit.header('breakfast menu')
+streamlit.header('*****Breakfast Menu*****')
 streamlit.text('Omega 3 & Blueberry oat meal')
 streamlit.text('kale, spinach & rocket smoothie')
 streamlit.text('hard-boiled free range egg')
