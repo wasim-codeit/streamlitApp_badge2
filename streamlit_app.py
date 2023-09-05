d3e276933529aa914f5daf29d3faca0a77ecf209
wasim-codeit <134153550+wasim-codeit@users.noreply.github.com>
2023-09-05 18:05:37 +0530

    Update streamlit_app.py
    ADDED EMOJI

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit
 
 streamlit.title('My hobby is to travel the world')
 
-streamlit.header('*****Breakfast Menu*****')
-streamlit.text('Omega 3 & Blueberry oat meal')
-streamlit.text('kale, spinach & rocket smoothie')
-streamlit.text('hard-boiled free range egg')
+streamlit.header('🍟 Breakfast Menu 🥪')
+streamlit.text('🥩 Omega 3 & Blueberry oat meal')
+streamlit.text('🥨 kale, spinach & rocket smoothie')
+streamlit.text('🥚 hard-boiled free range egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
