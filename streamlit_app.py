3845bc63a32d9b5d9e330a3aa8f08347b2de11e6
wasim-codeit <134153550+wasim-codeit@users.noreply.github.com>
2023-09-05 18:13:53 +0530

    Update streamlit_app.py
    reading csv file from s3 bucket using panda lib

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My hobby is to travel the world')
 
@@ -7,3 +8,7 @@ streamlit.text('🥩 Omega 3 & Blueberry oat meal')
 streamlit.text('🥨 kale, spinach & rocket smoothie')
 streamlit.text('🥚 hard-boiled free Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
