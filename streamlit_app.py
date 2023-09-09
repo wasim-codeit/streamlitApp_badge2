2a3b291f38724bff14365748b8e87764cef28e75
wasim-codeit <134153550+wasim-codeit@users.noreply.github.com>
2023-09-09 11:52:46 +0530

    Update streamlit_app.py
    new 9sept

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit
 import pandas
 
+streamlit.header("Fruityvice Fruit Advice!")
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
+
 streamlit.title('My hobby is to travel the world')
 streamlit.title('My skill is to serve the world to be healthy')
 
